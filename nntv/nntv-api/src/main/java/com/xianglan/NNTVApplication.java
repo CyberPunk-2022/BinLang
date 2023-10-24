72c6b9dfb227a497e8de4ca8197309bb8f1bff54
杨奇翰 <1215937291@qq.com>
2023-10-24 20:35:40 +0800

    构建后端项目，架构分层

---
+++ nntv/nntv-api/src/main/java/com/xianglan/NNTVApplication.java
@@ -0,0 +1,12 @@
+package com.xianglan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class NNTVApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NNTVApplication.class);
+    }
+}
